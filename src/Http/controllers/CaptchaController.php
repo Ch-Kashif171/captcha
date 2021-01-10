fde25a1930cbd28ac6faf66003a4a45ea3487c9c
Kashif Sohail <smalltime59@yahoo.com>
2021-01-10 20:59:32 +0500

    publish image

--- src/Http/controllers/CaptchaController.php
+++ src/Http/controllers/CaptchaController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace Kashif\Captcha\Http\Controllers;
+
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Kashif\Captcha\CaptchaBuilder;
 use Illuminate\Support\Facades\Session;
 
 class CaptchaController extends Controller {
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function reloadCaptcha()
     {
         Session::forget('k-captcha');
